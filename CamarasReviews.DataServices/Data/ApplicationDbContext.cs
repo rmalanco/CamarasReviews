694da177b988ba7e66a93605e031a031d8a46322
RMalanco <rmalanco@rmalanco.com>
2023-06-16 23:01:54 -0600

    Carga en el db conext

--- CamarasReviews.DataServices/Data/ApplicationDbContext.cs
+++ CamarasReviews.DataServices/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using CamarasReviews.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CamarasReviews.Data
@@ -9,5 +10,17 @@ namespace CamarasReviews.Data
             : base(options)
         {
         }
+        // Aqui se agregan los DbSet para cada modelo que se quiera agregar a la base de datos
+        DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        DbSet<BrandModel> Brands { get; set; }
+        DbSet<CategoryModel> Categories { get; set; }
+        DbSet<FeatureModel> Features { get; set; }
+        DbSet<ProductImageModel> ProductImages { get; set; }
+        DbSet<ProductModel> Products { get; set; }
+        DbSet<RatingModel> Ratings { get; set; }
+        DbSet<ReviewCommentModel> ReviewComments { get; set; }
+        DbSet<ReviewImageModel> ReviewImages { get; set; }
+        DbSet<ReviewModel> Reviews { get; set; }
+        DbSet<ReviewRatingModel> ReviewRatings { get; set; }
     }
 }
