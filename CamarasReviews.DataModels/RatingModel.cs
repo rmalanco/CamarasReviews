82bfa053bde62a1dbaa0edf2f21c540af246be3b
RMalanco <rmalanco@rmalanco.com>
2023-06-16 22:52:18 -0600

    update

--- CamarasReviews.DataModels/RatingModel.cs
+++ CamarasReviews.DataModels/RatingModel.cs
@@ -1,6 +1,14 @@
-﻿namespace CamarasReviews.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CamarasReviews.Models
 {
     public class RatingModel
     {
+        [Key]
+        public Guid RatingId { get; set; }
+        [Required]
+        public string RatingName { get; set; }
+        [Required]
+        public int RatingValue { get; set; }
     }
 }
--- CamarasReviews.DataModels/ReviewRatingModel.cs
+++ CamarasReviews.DataModels/ReviewRatingModel.cs
@@ -15,15 +15,4 @@ public class ReviewRatingModel
     public Guid RatingId { get; set; }
     [ForeignKey("RatingId")]
     public RatingModel Rating { get; set; }
-    [Display(Name = "Fecha de Creacion")]
-    [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy hh:mm tt}", ApplyFormatInEditMode = false)]
-    public DateTime CreatedDate { get; set; } = DateTime.Now;
-    [Display(Name = "Fecha de Modificacion")]
-    [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy hh:mm tt}", ApplyFormatInEditMode = false)]
-    public DateTime? ModifiedDate { get; set; }
-    [Display(Name = "Fecha de Eliminacion")]
-    [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy hh:mm tt}", ApplyFormatInEditMode = false)]
-    public DateTime? DeletedDate { get; set; }
-    [Display(Name = "Activo")]
-    public bool IsActive { get; set; }
 }
